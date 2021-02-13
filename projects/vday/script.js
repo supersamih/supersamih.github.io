430ef272095b99eb99988386ed3d8378df0b90b3
supersamih <tiltlord@gmail.com>
2021-02-13 12:21:56 +0000

    vday done

--- projects/vday/script.js
+++ projects/vday/script.js
@@ -68,7 +68,7 @@ const textNodes = [
     ],
   },
   {
-    id: 2, //flirt or no flirt scenario
+    id: 2,
     text: `Nataly looked back towards the burning wreck of the ship, reflections of flame dancing in the midnight ocean as it slowly sank beneath the waves. How long had it been since they ran aground? Impossible to say, but it felt like an eternity. Caught up in the storm, the S.S Chee-z had been tossed like a rag doll towards the inescapable, dark monolith that now lay inexorably before them. Thrown against the rocks, she supposed that at some point the cargo must have spilled loose and been ignited by a stray spark. She looked to her sides, taking in the full length of the dark beach and the cliffs and forest that lay further inland. This island was an unknown, something which had been absent from all the latest navigational charts.
 
       To Nataly’s side, Naseem ran his hands through his luscious locks of hair. She was struck suddenly by how unbelievably handsome he was, and found herself blushing. `,
@@ -81,12 +81,12 @@ const textNodes = [
       {
         text: "Don't Flirt",
         setState: { noflirt: true },
-        nextText: 4, //go ot no flirt scenario
+        nextText: 4,
       },
     ],
   },
   {
-    id: 101, //flirt or no flirt scenario
+    id: 101,
     text: `Naseem looked back towards the burning wreck of the ship, reflections of flame dancing in the midnight ocean as it slowly sank beneath the waves. How long had it been since they ran aground? Impossible to say, but it felt like an eternity. Caught up in the storm, the S.S Chee-z had been tossed like a rag doll towards the inescapable, dark monolith that now lay inexorably before them. Thrown against the rocks, he supposed that at some point the cargo must have spilled loose and been ignited by a stray spark. He looked to his sides, taking in the full length of the dark beach and the cliffs and forest that lay further inland. This island was an unknown, something which had been absent from all the latest navigational charts.
     To Naseem’s side, Nataly looked kinda sus. Noticing his glance, she asked him ‘what is it?’. Naseem was suddenly struck by how amazingly attractive and seductive her voice was.
 `,
@@ -99,12 +99,12 @@ const textNodes = [
       {
         text: "Don't Flirt",
         setState: { noflirt: true },
-        nextText: 104, //go ot no flirt scenario
+        nextText: 104,
       },
     ],
   },
   {
-    id: 104, // NO flirt scenario
+    id: 104,
     text: `Turning to face the forest, Naseen began considering what their next move should be. In the distance he could faintly make out what could possibly be smoke and a faint light from a fire deep in the depths.
     ‘Nataly, it looks like there might be someone else out there. Maybe we should go check it out?’
     Nataly nodded, and the two began to walk into the darkness of the forest. For hours, they travelled by torchlight along well-trodden ancient paths. With no guide nor any landmarks to guide them, they found themselves often lost, wandering in circles. By chance, they stumbled their way through the brush and directly into a large tribal settlement, running directly into what appeared to be a primitive-looking man dressed in tribal furs. His features were exaggerated, almost like the drawings of neanderthal cavemen that Naseem had seen in books. As he spotted the two, he pointed and screamed out.
@@ -122,7 +122,7 @@ const textNodes = [
     ],
   },
   {
-    id: 4, // NO flirt scenario
+    id: 4,
     text: `Turning to face the forest, Nataly began considering what their next move should be. In the distance she could faintly make out what could possibly be smoke and a faint light from a fire deep in the depths.
     ‘Naseem, it looks like there might be someone else out there. Maybe we should go check it out?’
     Naseem nodded, and the two began to walk into the darkness of the forest. For hours, they travelled by torchlight along well-trodden ancient paths. With no guide nor any landmarks to guide them, they found themselves often lost, wandering in circles. By chance, they stumbled their way through the brush and directly into a large tribal settlement, running directly into what appeared to be a primitive-looking man dressed in tribal furs. His features were exaggerated, almost like the drawings of neanderthal cavemen that Naseem had seen in books. As he spotted the two, he pointed and screamed out.
@@ -140,7 +140,7 @@ const textNodes = [
     ],
   },
   {
-    id: 103, //YES flirt / Ambush scenario
+    id: 103,
     text: `Nataly, I know this isn’t the time, but...damn you got a sexy voice,’
     ‘Wh-what do you mean? Idiot!’ she replied, punching him in the arm. But her embarrassed expression as she awkwardly looked away revealed her true feelings.
 
@@ -166,7 +166,7 @@ const textNodes = [
     ],
   },
   {
-    id: 3, //YES flirt / Ambush scenario
+    id: 3,
     text: `‘Naseem, I know this isn’t the time, but...damn you’re pretty’
           ‘I don’t know Nataly, kinda sus’, he replied.
 
@@ -192,7 +192,7 @@ const textNodes = [
     ],
   },
   {
-    id: 105, //ambush scenario bad responce
+    id: 105,
     text: `The tribesman’s face contorted in fury as he heard the reply. Leaping forwards with a screech of fury, he brought his spear down.
     On this signal the rest of the tribespeople rush Naseem and Nataly killing them instantaneously`,
     options: [
@@ -207,7 +207,7 @@ const textNodes = [
     ],
   },
   {
-    id: 5, //ambush scenario bad responce
+    id: 5,
     text: `The response sent the tribesmen into a frenzy of monkey-like grunting. As they raised their weapons and charged forwards, Naseem stepped in front of Nataly, and moving into a well-practiced boxing stance, he deftly dodged the spears of the foremost two tribesmen and knocked them flat with blows to the head. Before he could brace himself for the next wave, however, he found himself surrounded on all sides by spears. Lowering his fists, he was approached by the leader of the group.
     ‘You come us now. Be prisoner. Me Glimpee, do best capture.’
     Naseem and Nataly found themselves tied by the group of tribesmen and pulled into the forest. After what felt like hours of relentless marching, they saw a settlement ahead.
@@ -224,7 +224,7 @@ const textNodes = [
     ],
   },
   {
-    id: 6, //ambush scenario good responce scenario
+    id: 6,
     text: `‘Come this way my broodas’ the lead tribesmen replied. ‘Me call Glimpee, take see Pepega leader, wise man, they help’.
     Nataly turned to Naseem and raised an eyebrow.
     ‘I suppose we don’t have any better leads. It seems like they’ve lived here for a long time. Maybe they know something about this place?’
@@ -244,7 +244,7 @@ const textNodes = [
     ],
   },
   {
-    id: 7, // VILLAGE SCENARIO
+    id: 7,
     text: `Arriving at the village, the duo looked around at an expansive clearing filled with ramshackle wooden huts. The chaos was organised around a large, exquisitely carved throne which contrasted starkly with the messy nature of the rest of the village. Atop the throne sat what must surely have been the tribe leader. Her short stature belied the majesty that surrounded her. 
     ‘Greetings my fellow Pepegas,’ she exclaimed. Casting her eyes upon Naseem and Nataly, she added ‘what is this we have in our midst today, Glimpee?’
     ‘Ooo oo ooo ee ooh,’ the tribesman known as Glimpee responded. Somehow, the leader seemed to understand his nonsensical grunting perfectly, and replied in the same way.
@@ -395,32 +395,36 @@ const textNodes = [
     `,
     options: [
       {
-        text: "Continue", //moka
+        text: "Continue",
         requiredState: (currentState) => currentState.moka,
+        setState: { mokadead: true },
         nextText: 11,
       },
       {
-        text: "Continue", //moka
+        text: "Continue",
         requiredState: (currentState) => currentState.moka,
+        setState: { mokadead: true },
         nextText: 11,
       },
       {
-        text: "Continue", // glimpee
+        text: "Continue",
         requiredState: (currentState) => currentState.glimpee,
+        setState: { glimpeedead: true },
         nextText: 12,
       },
       {
-        text: "Continue", // glimpee
+        text: "Continue",
         requiredState: (currentState) => currentState.glimpee,
+        setState: { glimpeedead: true },
         nextText: 12,
       },
       {
-        text: "Continue", // ie
+        text: "Continue",
         requiredState: (currentState) => currentState.ie,
         nextText: 13,
       },
       {
-        text: "Continue", // ie
+        text: "Continue",
         requiredState: (currentState) => currentState.ie,
         nextText: 13,
       },
@@ -541,71 +545,9 @@ const textNodes = [
     ],
   },
   {
-    id: 15,
-    text: `With a battle cry Naseem charged the evil Pogge. His spell fired off, sending searing black flames directly towards him.
-    At the last moment, Nataly leaped in front of the flames, taking them directly to the chest. As her body and soul were seared by the dark magic, Naseem drove his Pepega spear directly into Pogge’s chest.`,
-    options: [
-      {
-        text: "Continue",
-        nextText: 20,
-      },
-      {
-        text: "Continue",
-        nextText: 20,
-      },
-    ],
-  },
-  {
-    id: 16,
-    text: `With a battle cry Nataly charged the evil Pogge. His spell fired off, sending searing black flames directly towards her.
-    At the last moment, Naseem leaped in front of the flames, taking them directly to the chest. As his body and soul were seared by the dark magic, Nataly drove her Pepega spear directly into Pogge’s chest.`,
-    options: [
-      {
-        text: "Continue",
-        nextText: 20,
-      },
-      {
-        text: "Continue",
-        nextText: 20,
-      },
-    ],
-  },
-  {
-    id: 17,
-    text: `With a battle cry Nataly charged the evil Pogge. His spell fired off, sending searing black flames directly towards her.
-    The flames hit Nataly directly in the chest, searing into her very soul. As her life faded, Naseem charged Pogge with his Pepega spear, driving it directly into his chest.
-`,
-    options: [
-      {
-        text: "Continue",
-        nextText: 20,
-      },
-      {
-        text: "Continue",
-        nextText: 20,
-      },
-    ],
-  },
-  {
-    id: 18,
-    text: `With a battle cry Naseem charged the evil Pogge. His spell fired off, sending searing black flames directly towards him.
-    The flames hit Naseem directly in the chest, searing into his very soul. As his life faded, Nataly charged Pogge with her Pepega spear, driving it directly into his chest.
-`,
-    options: [
-      {
-        text: "Continue",
-        nextText: 20,
-      },
-      {
-        text: "Continue",
-        nextText: 20,
-      },
-    ],
-  },
-  {
-    id: 19, // dodge once scenario
+    id: 19,
     text: `Nataly and Naseem rolled to each side, deftly avoiding the gout of dark flame fired by the evil Pogge.
-    Pogge scoffs and laughs and begins drawing power from his crystal another of his dark spells.`,
+    Pogge scoffs and laughs and begins drawing power from his crystal to shoot another of his dark spells.`,
     options: [
       {
         text: "Attack with Pepega Spear",
@@ -653,30 +595,102 @@ const textNodes = [
     ],
   },
   {
+    id: 15,
+    text: `With a battle cry Naseem charged the evil Pogge. His spell fired off, sending searing black flames directly towards him.
+    At the last moment, Nataly leaped in front of the flames, taking them directly to the chest. As her body and soul were seared by the dark magic, Naseem drove his Pepega spear directly into Pogge’s chest.`,
+    options: [
+      {
+        text: "Continue",
+        requiredState: (currentState) => currentState.glimpeedead,
+        nextText: 201, // nataly dead glimpee dead
+      },
+      {
+        text: "Continue",
+        requiredState: (currentState) => currentState.mokadead,
+        nextText: 202, // nataly dead moka dead
+      },
+    ],
+  },
+  {
+    id: 16,
+    text: `With a battle cry Nataly charged the evil Pogge. His spell fired off, sending searing black flames directly towards her.
+    At the last moment, Naseem leaped in front of the flames, taking them directly to the chest. As his body and soul were seared by the dark magic, Nataly drove her Pepega spear directly into Pogge’s chest.`,
+    options: [
+      {
+        text: "Continue",
+        requiredState: (currentState) => currentState.glimpeedead,
+        nextText: 203, // naseem dead glim dead
+      },
+      {
+        text: "Continue",
+        requiredState: (currentState) => currentState.mokadead,
+        nextText: 204, //naseem dead moka ded
+      },
+    ],
+  },
+  {
+    id: 17,
+    text: `With a battle cry Nataly charged the evil Pogge. His spell fired off, sending searing black flames directly towards her.
+    The flames hit Nataly directly in the chest, searing into her very soul. As her life faded, Naseem charged Pogge with his Pepega spear, driving it directly into his chest.
+`,
+    options: [
+      {
+        text: "Continue",
+        requiredState: (currentState) => currentState.glimpeedead,
+        nextText: 201, // nataly dead glimpee dead
+      },
+      {
+        text: "Continue",
+        requiredState: (currentState) => currentState.mokadead,
+        nextText: 202, // nataly dead moka dead
+      },
+    ],
+  },
+  {
+    id: 18,
+    text: `With a battle cry Naseem charged the evil Pogge. His spell fired off, sending searing black flames directly towards him.
+    The flames hit Naseem directly in the chest, searing into his very soul. As his life faded, Nataly charged Pogge with her Pepega spear, driving it directly into his chest.
+`,
+    options: [
+      {
+        text: "Continue",
+        requiredState: (currentState) => currentState.glimpeedead,
+        nextText: 203, // naseem dead glim dead
+      },
+      {
+        text: "Continue",
+        requiredState: (currentState) => currentState.mokadead,
+        nextText: 204, //naseem dead moka ded
+      },
+    ],
+  },
+  {
     id: 20,
     text: `With a battle cry Nataly charged the evil Pogge. His spell fired off, sending searing black flames directly towards her. At the last moment, however, her glowing sword absorbed the flame like a magnet. Continuing with her momentum, Nataly thrust the sword directly through Pogge’s chest in a flash of holy light.`,
     options: [
       {
         text: "Continue",
-        nextText: 999,
+        nextText: 205, // both live
       },
       {
         text: "Continue",
-        nextText: 999,
+        requiredState: (currentState) => currentState.flirt,
+        nextText: 206,
       },
     ],
   },
   {
     id: 21,
-    text: `With a battle cry Nataly charged the evil Pogge. His spell fired off, sending searing black flames directly towards her. At the last moment, however, her glowing sword absorbed the flame like a magnet. Continuing with her momentum, Nataly thrust the sword directly through Pogge’s chest in a flash of holy light.`,
+    text: `With a battle cry Naseem charged the evil Pogge. His spell fired off, sending searing black flames directly towards Naseem. At the last moment, however, the glowing sword absorbed the flame like a magnet. Continuing with his momentum, Naseem thrust the sword directly through Pogge’s chest in a flash of holy light.`,
     options: [
       {
         text: "Continue",
-        nextText: 999,
+        nextText: 205, // both live
       },
       {
         text: "Continue",
-        nextText: 999,
+        requiredState: (currentState) => currentState.flirt,
+        nextText: 206,
       },
     ],
   },
@@ -686,11 +700,8 @@ const textNodes = [
     options: [
       {
         text: "Continue",
-        nextText: 999,
-      },
-      {
-        text: "Continue",
-        nextText: 999,
+        requiredState: (currentState) => currentState.flirt,
+        nextText: 206,
       },
     ],
   },
@@ -702,11 +713,109 @@ const textNodes = [
     options: [
       {
         text: "Continue",
-        nextText: 999,
+        nextText: 207, // both live pogge good
+      },
+    ],
+  },
+  {
+    id: 201,
+    text: `With Pogge defeated, Naseem returned to the Pepega tribe’s village. With the tribe’s bravest warrior gone, the Pepegas mourned for their lost friend and struggled to ensure their food supplies. Nonetheless, they celebrated the defeat of the evil wizard, their newfound freedom, and their two heroes.
+    `,
+    options: [
+      {
+        text: "Thanks for playing",
+        nextText: -1,
       },
       {
-        text: "Continue",
-        nextText: 999,
+        text: "Play again?",
+        nextText: -1,
+      },
+    ],
+  },
+  {
+    id: 202,
+    text: `With Pogge defeated, Naseem returned to the Pepega tribe’s village. With the tribe’s leader gone, the Pepegas reverted to (even more) primitive ways, splitting apart into factions and warring against each other.`,
+    options: [
+      {
+        text: "Thanks for playing",
+        nextText: -1,
+      },
+      {
+        text: "Play again?",
+        nextText: -1,
+      },
+    ],
+  },
+  {
+    id: 203,
+    text: `With Pogge defeated, Nataly returned to the Pepega tribe’s village. With the tribe’s bravest warrior gone, the Pepegas mourned for their lost friend and struggled to ensure their food supplies. Nonetheless, they celebrated the defeat of the evil wizard, their newfound freedom, and their two heroes.
+    `,
+    options: [
+      {
+        text: "Thanks for playing",
+        nextText: -1,
+      },
+      {
+        text: "Play again?",
+        nextText: -1,
+      },
+    ],
+  },
+  {
+    id: 204,
+    text: `With Pogge defeated, Nataly returned to the Pepega tribe’s village. With the tribe’s leader gone, the Pepegas reverted to (even more) primitive ways, splitting apart into factions and warring against each other.`,
+    options: [
+      {
+        text: "Thanks for playing",
+        nextText: -1,
+      },
+      {
+        text: "Play again?",
+        nextText: -1,
+      },
+    ],
+  },
+  {
+    id: 205,
+    text: `With Pogge defeated, Nataly and/or Naseem returned to the Pepega tribe’s village, where they spent the rest of their lives living peacefully amongst their fellow tribesmen.
+    `,
+    options: [
+      {
+        text: "Thanks for playing",
+        nextText: -1,
+      },
+      {
+        text: "Play again?",
+        nextText: -1,
+      },
+    ],
+  },
+  {
+    id: 206,
+    text: `With Pogge defeated, Nataly and/or Naseem returned to the Pepega tribe’s village, where they spent the rest of their lives living peacefully amongst their fellow tribesmen. The two married and produced many happy children. :)
+    `,
+    options: [
+      {
+        text: "Thanks for playing",
+        nextText: -1,
+      },
+      {
+        text: "Play again?",
+        nextText: -1,
+      },
+    ],
+  },
+  {
+    id: 207,
+    text: `With Pogge convinced, the group returned to the Pepega tribe’s village. With their help, they were able to fashion a basic raft. Using Pogge’s magic, the raft survived the journey across the ocean and Nataly and Naseem were able to return to their lives. Pogge decided to devote his magic to helping people, and in his downtime he became an extremely popular and successful elderly Twitch streamer`,
+    options: [
+      {
+        text: "Thanks for playing",
+        nextText: -1,
+      },
+      {
+        text: "Play again?",
+        nextText: -1,
       },
     ],
   },
